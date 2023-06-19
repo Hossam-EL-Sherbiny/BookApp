a7bc588ee73b425e4feef21b2479b0fcaae82601
i7-1165G7 <i7-1165G7@DESKTOP-RMU0F0G>
2023-06-19 14:13:58 +0300

    Add project files.

---
+++ BookApp/Database.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace BookApp
+{
+    public class Database
+    {
+        private List<Book> books;
+        public Database()
+        {
+            // Initialize the books list as a new empty list.
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book addBook)
+        {
+            addBook.Id = GetNextId();
+            books.Add(addBook); // adds the book to the books list.
+        }
+
+        public void UpdateBook(Book update)
+        {
+            Book existingBook = books.FirstOrDefault(b => b.Id == update.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = update.Title;
+                existingBook.Author = update.Author;
+                existingBook.Year = update.Year;
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            Book deleteBook = books.FirstOrDefault(b => id == id);
+            if (deleteBook != null) 
+                books.Remove(deleteBook);
+        }
+
+        public Book GetBook(int id)
+        {
+            return books.FirstOrDefault(b => b.Id == id);
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        private int GetNextId()
+        {
+            return books.Count > 0 ? books.Max(b => b.Id) + 1 : 1;
+        }
+    }
+}
---
+++ BookApp/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BookApp;
+
+Database db = new Database();
+
+// Add some books to the database..
+db.AddBook(new Book { Title = "To Kill a Mockingbrid", Author = "Harper Lee", Year = 1960});
+db.AddBook(new Book { Title = "1984", Author = "George Orwell", Year = 1949 });
+db.AddBook(new Book { Title = "Pride and Prejudice", Author = "Jane Austen", Year = 1813 });
+
+
+Book book = db.GetBook(2);
+if (book != null)
+{
+    Console.WriteLine("Book details: ");
+    Console.WriteLine($"ID: {book.Id}");
+    Console.WriteLine($"Title: {book.Title}");
+    Console.WriteLine($"Author: {book.Author}");
+    Console.WriteLine($"Year: {book.Year}");
+
+    Book bookToUpdate = db.GetBook(3);
+    if (bookToUpdate != null)
+    {
+        bookToUpdate.Title = "Sense and Sensibility";
+        bookToUpdate.Author = "ane Austen";
+        bookToUpdate.Year = 1811;
+
+        db.UpdateBook(bookToUpdate);
+        Console.WriteLine("Updated Book details: ");
+        Console.WriteLine($"ID: {bookToUpdate.Id}");
+        Console.WriteLine($"Title: {bookToUpdate.Title}");
+        Console.WriteLine($"Author: {bookToUpdate.Author}");
+        Console.WriteLine($"Year: {bookToUpdate.Year}");
+    }
+
+    // Delete a book and print the remaining books
+    db.DeleteBook(1);
+
+    Console.WriteLine("Remaining books:");
+    List<Book> allBooks = db.GetAllBooks();
+    foreach (Book b in allBooks)
+    {
+        Console.WriteLine($"ID: {b.Id}, Title: {b.Title}, Author: {b.Author}, Year: {b.Year}");
+    }
+
+    Console.ReadKey();
+}
